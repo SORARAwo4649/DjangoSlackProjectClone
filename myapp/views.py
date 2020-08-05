6d5302d11422056439b47c54a35f7413ec22d377
SORARAwo4649 <k11274649@icloud.com>
2020-08-05 23:48:27 +0900

    fix little

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-import urllib.request
-import json
 from slack import WebClient
 from slack.errors import SlackApiError
 import certifi

7e0777606cb399fa51aaaf2d53c2d62c23337b68
SORARAwo4649 <k11274649@icloud.com>
2020-08-12 23:13:18 +0900

    edit slack app

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gs(-i37u#=#wuhb5rvktf^t%_d*r4!4*jkydlru@#9xc#aj2%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView
 from slack import WebClient
 from slack.errors import SlackApiError
 import certifi
@@ -9,39 +9,71 @@ import ssl as ssl_lib
 from .forms import NameForm
 
 
-class CreatingChannels(View):
-    def creating_channels(self, names):
-        try:
-            token = 'xoxb-1287291111699-1287326217875-T403vFOqTiXz40rNHAmzErYG'
-            # token = 'xoxb-1265411682005-1269328032261-XsGAtpe0jSLhnpqEBSOTZjBZ'
-            ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-            client = WebClient(
-                token=f"{token}", ssl=ssl_context
-            )
+class CreatingChannels:
+    def __init__(self, names, plan, date1, date2, date3):
+        # 初期設定***************************************************************
+        token = 'xoxb-1287291111699-1302304008356-f1tUrFxULMdzFnoMONhoRHk5'
+        self.users_id = 'U0180A2FVKR'
+        # *********************************************************************
 
+        print("start!!")
+        self.names = names
+        self.plan = plan
+        self.date1 = date1
+        self.date2 = date2
+        self.date3 = date3
+
+        # 開発用
+        ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+        self.client = WebClient(
+            token=f"{token}", ssl=ssl_context
+        )
+
+    def creating_channels(self):
+        try:
             # チャンネルの作成
-            channel_name = f"メンター部屋-{names}様"
-            response = client.conversations_create(
+            channel_name = f"メンター部屋-{self.names}様"
+            response = self.client.conversations_create(
                 name=channel_name,
                 is_private=True,
             )
             channel_id = response["channel"]["id"]
 
-            # 定型文の送信
-            response_chat = client.chat_postMessage(
-                channel=f"{channel_id}",
-                text=f"管理者からのリクエストで、ボットが{names}様のメンター部屋を自動作成しました。\n※ボットの退出後にこのボットが再入室することは出来ません。またボット経由で第三者がメッセージのやり取りを見ることも出来ませんのでご安心ください。"
-            )
+            # 順次実行する
+            self.sending_message(channel_id)
+            self.inviting_user(channel_id)
+            self.leaving_user(channel_id)
 
-            # チャンネルへの招待
-            # users_id = "U0187C0V4BB"
-            users_id = 'U0180A2FVKR'
-            response_invite = client.conversations_invite(
+        except SlackApiError as e:
+            print(e)
+            return e.response["ok"]
+
+    def sending_message(self, channel_id):
+        try:
+            # 定型文の送信
+            self.client.chat_postMessage(
                 channel=f"{channel_id}",
-                users=f"{users_id}",
+                text=f"管理者からのリクエストで、ボットが{self.names}様のメンター部屋を自動作成しました。\n※ボットの退出後にこのボットが再入室することは出来ません。またボット経由で第三者がメッセージのやり取りを見ることも出来ませんのでご安心ください。"
             )
+        except SlackApiError as e:
+            print(e)
+            return e.response["ok"]
+
+    def inviting_user(self, channel_id):
+        try:
+            # チャンネルへの招待
+            self.client.conversations_invite(
+                channel=f"{channel_id}",
+                users=f"{self.users_id}",
+            )
+        except SlackApiError as e:
+            print(e)
+            return e.response["ok"]
+
+    def leaving_user(self, channel_id):
+        try:
             # ボットをチャンネルから退出させる
-            response_leave = client.conversations_leave(
+            self.client.conversations_leave(
                 channel=f"{channel_id}",
             )
         except SlackApiError as e:
@@ -64,9 +96,16 @@ class CreatingView(TemplateView):
         context = {
             'name': request.POST['name']
         }
+        # 入力フォームから値を持ってくる
         names = request.POST['name']
-        slack_channels = CreatingChannels()
-        name_check = slack_channels.creating_channels(names)
+        plan = request.POST['plan']
+        date1 = request.POST['date1']
+        date2 = request.POST['date2']
+        date3 = request.POST['date3']
+
+        # Slack API を起動する
+        slack_channels = CreatingChannels(names, plan, date1, date2, date3)
+        name_check = slack_channels.creating_channels()
         print(name_check)
         # 規約違反だけどnotにするとNoneも判定されるため使えない
         if name_check == False:
